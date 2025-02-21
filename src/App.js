ac25d3eceb98b6cd670fde1012c2daad598fb376
leeseayoung <tnfusrltk12@naver.com>
2025-02-21 16:04:02 +0900

    5장 함수형 컴포넌트

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import ValidationSample from "./ValidationSample";
+import React, { useRef } from "react";
+import ScrollBox from "./ScrollBox";
 
 function App() {
+  const scrollBoxRef = useRef();
+
   return (
-    <>
-      <ValidationSample />
-    </>
+    <div>
+      <ScrollBox ref={scrollBoxRef} />
+      <button onClick={() => scrollBoxRef.current.scrollToBottom()}>
+        맨 밑으로
+      </button>
+    </div>
   );
 }
 
